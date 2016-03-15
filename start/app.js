26e57cd85cfe6e2fc8f1581275ac1c5fa39efed9
Cody Rice <cody.rice@gmail.com>
2016-03-15 06:31:23 +0100

    Fixed dependency errors

--- start/app.js
+++ start/app.js
@@ -1,14 +1,17 @@
+
+// iniitalizing dependency variables
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var helmet = require('helmet');
+require('dotenv').load();
+
+var app = express();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,11 +20,12 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 
+// setting routes
 app.use('/', routes);
 app.use('/users', users);
 
@@ -34,27 +38,14 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (error, request, response, next) {
+    response.status(error.status || 500);
+    response.json({ error: error.message });
 });
 
+var server = app.listen(3000, 'localhost', function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-module.exports = app;
+    console.log('App is listening on http://%s:%s', host, port);
+});
