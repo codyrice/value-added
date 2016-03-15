9a703be23f5bdab8de80e88a4a52429e4d4d397a
Cody Rice <cody.rice@gmail.com>
2016-03-15 18:54:14 +0100

    Removed FB login and updated index title

--- start/routes/index.js
+++ start/routes/index.js
@@ -5,17 +5,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Valuable Support' });
 });
 
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
-});
-
-/* GET Hello World page. */
-router.get('/facebook', function(req, res) {
-  res.render('facebook', { title: 'Facebook Login!' });
 });
 
 router.get('/userlist', auth.authorize, function (request, response) {
